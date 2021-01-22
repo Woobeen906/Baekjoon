17ea681dbfa53f967962f35d8bfb1fe2e8a4f7c0
Woobeen906 <67691520+Woobeen906@users.noreply.github.com>
2021-01-22 20:09:54 +0900

    피보나치 수 2

---
+++ 2748.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+long long int dp[91];
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	dp[1]= dp[2] = 1;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[n];
+}
