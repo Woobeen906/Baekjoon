127b4c3838960f68489988e61bbd61fc576bfb3a
Woobeen906 <67691520+Woobeen906@users.noreply.github.com>
2021-02-04 17:20:57 +0900

    배열 합치기

--- 11728.cpp
+++ 11728.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 	int n, m;
 	cin >> n >> m;
-	int arr[200001] = { 0, };
+	int arr[2000001] = { 0, };
 	for (int i = 0; i < n+m; i++) {
 		cin >> arr[i];
 	}
