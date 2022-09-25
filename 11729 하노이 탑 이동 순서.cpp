02bcc157e920e804086243b71d68c86a7771d9c7
Woobeen906 <siugan@naver.com>
2022-09-25 20:42:28 +0900

    11729 하노이 탑 이동 순서

---
+++ 11729 하노이 탑 이동 순서.cpp
@@ -0,0 +1,39 @@
+//
+//  11729 하노이 탑 이동 순서.cpp
+//  acmicpc
+//
+//  Created by 박우빈 on 2022/09/25.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n,k;
+vector<pair<int, int>>v;
+
+void move(int start,int end,int block){
+    
+    v.push_back(make_pair(start, end));
+    k++;
+}
+
+void hanoi(int start,int mid,int end,int block){
+    if(block==1){
+        move(start,end,block);
+        return;
+    }
+    hanoi(start,end,mid,block-1);
+    move(start,end,block);
+    hanoi(mid,start,end,block-1);
+}
+
+
+int main(){
+    cin>>n;
+    
+    hanoi(1,2,3,n);
+    cout<<k<<"\n";
+    for(int i=0;i<k;i++)cout<<v[i].first<<" "<<v[i].second<<"\n";
+}
